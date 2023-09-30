6847bfe7970cdb2a0b6473f179ff47eaa5f79352
Nanii <breadnanii@gmail.com>
2023-10-01 00:35:46 +0900

    FEAT: FilterBar 컴포넌트 생성
    - useFocus 커스텀 훅을 생성하였습니다.
    - input에 포커스되면 border 스타일을 변경합니다.

---
+++ src/hook/useFocus.tsx
@@ -0,0 +1,12 @@
+import { useState, useCallback } from "react"
+
+const useFocus = () => {
+  const [isFocus, setIsFocus] = useState(false)
+
+  const onFocus = useCallback(() => setIsFocus(true), []);
+  const onBlur = useCallback(() => setIsFocus(false), []);
+
+  return { isFocus, onFocus, onBlur }
+}
+
+export default useFocus;
