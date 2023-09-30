04cfe5a9ef8ad89246af41bed94cb69c3ff528a2
Nanii <breadnanii@gmail.com>
2023-09-30 19:28:47 +0900

    FIX: Auth route 에러 수정
    - authOptions 분리하여
    - Auth Route type error 를 수정하였습니다.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,5 @@
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth/next";
-import GithubProvider from 'next-auth/providers/github';
 
-export const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENTID || '',
-      clientSecret: process.env.GITHUB_SECRET|| ''
-    }),
-  ]
-});
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nanum_Gothic_Coding } from 'next/font/google'
+import AuthSessionContext from '@/context/AuthSessionContext';
+import Header from '@/components/Common/Header';
 
 const nanum = Nanum_Gothic_Coding({ subsets: ['latin'], weight: ['400', '700'] });
-
 
 export const metadata: Metadata = {
   title: 'Issue Tracker - Next.js',
@@ -16,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
-      <body className={nanum.className}>{children}</body>
+    <html lang="ko" className={nanum.className}>
+      <body className='w-full max-w-screen-xl overflow-auto mx-auto '>
+        <AuthSessionContext>
+          <Header />
+          {children}
+        </AuthSessionContext>
+      </body>
     </html>
   )
 }
