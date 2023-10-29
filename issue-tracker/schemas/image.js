a653cee3d087c3607d6c2a10d68794730b036c57
Nanii <breadnanii@gmail.com>
2023-10-30 00:32:44 +0900

    CHORE: Versioning untracked files

---
+++ issue-tracker/schemas/image.js
@@ -0,0 +1,32 @@
+export default {
+  title: 'Image',
+  name: 'image',
+  type: 'document',
+  fields: [
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image',
+    },
+    {
+      title: 'AssetId',
+      name: 'assetId',
+      type: 'string',
+    },
+    {
+      title: 'Path',
+      name: 'path',
+      type: 'string',
+    },
+    {
+      title: 'Filename',
+      name: 'filename',
+      type: 'string',
+    },
+    {
+      title: 'Url',
+      name: 'url',
+      type: 'string',
+    },
+  ],
+}
